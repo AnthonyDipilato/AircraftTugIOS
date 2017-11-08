9cc7a7b9aefb1a86c610846424c5d6bb02c85df0
Anthony Dipilato <anthonydipilato@anthonys-mbp>
2017-11-07 21:52:57 -0500

    Initial Commit

---
+++ AircraftTug/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AircraftTug
+//
+//  Created by Anthony Dipilato on 11/7/17.
+//  Copyright © 2017 Bumbol Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
